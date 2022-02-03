d8c14958ac8d4d43bc2bb34c447811eea5e07747
vagrant <wong_michael@rogers.com>
2022-02-03 03:09:18 +0000

    Set up databases, add tables and insert info

---
+++ db/seeds/04_insertSubmissions.sql
@@ -0,0 +1,24 @@
+
+-- poll submissions for Japanese
+INSERT INTO submissions (choice_id, user_id, point) VALUES (1, 1, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (1, 2, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (1, 3, 3);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (1, 4, 4);
+
+-- poll submissions for Italian
+INSERT INTO submissions (choice_id, user_id, point) VALUES (2, 1, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (2, 2, 3);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (2, 3, 4);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (2, 4, 1);
+
+-- poll submissions for Chinese
+INSERT INTO submissions (choice_id, user_id, point) VALUES (3, 1, 3);
+INSERT INTO submissions (choice_id, user_id, point) VALUES ( 3, 2, 4);
+INSERT INTO submissions (choice_id, user_id, point) VALUES ( 3, 3, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES ( 3, 4, 2);
+
+-- poll submissions for Korean
+INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 1, 4);
+INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 2, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 3, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 4, 3);
