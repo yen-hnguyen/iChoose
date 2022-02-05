384e699d08b351416e7cc41f1a4a6d4da94e9f68
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-02-05 10:59:59 -0500

    convert to jQuery code in recent_polls js

--- public/scripts/recent_polls.js
+++ public/scripts/recent_polls.js
@@ -1,34 +1,33 @@
 import { assignChartParams } from "./chart_params.js"
 
-const polls = document.getElementById("chosen-polls");
-polls.style.display = "flex";
-polls.style.justifyContent = "space-around";
-polls.innerHTML = `
-<div class="recent-polls" style="width: 425px;">
-  <h5 id="title1"></h5>
-  <canvas id="chart1" width="400" height="400"></canvas>
-</div>
-<div class="recent-polls" style="width: 425px;">
-  <h5 id="title2"></h5>
-  <canvas id="chart2" width="400" height="400"></canvas>
-</div>
-`;
+$(document).ready(function () {
 
-const title1 = "Which movie to watch on the weekend?";
-document.getElementById("title1").append(title1);
-const title2 = "What's the lunch plan on Friday?";
-document.getElementById("title2").append(title2);
+  const polls = $("#chosen-polls");
+  const pollElement = `
+  <div class="recent-polls" style="width: 425px;">
+   <h5 id="title1">Which movie to watch on the weekend?</h5>
+   <canvas id="chart1" width="400" height="400"></canvas>
+  </div>
+  <div class="recent-polls" style="width: 425px;">
+   <h5 id="title2">What's the lunch plan on Friday?</h5>
+   <canvas id="chart2" width="400" height="400"></canvas>
+  </div>
+  `;
+  polls.append(pollElement);
+  polls.css({"display": "flex", "justify-content": "space-around"});
 
-const movieLabels = ["Matrix Revolutions", "Spiderman", "John Wick 4", "Avatar 2"];
-const pollVotes1 = [5, 6, 4, 7];
-const foodLabels = ["Sushi", "Tacos", "Pizza", "Ramen"];
-const pollVotes2 = [5, 6, 4, 7];
+  const movieLabels = ["Matrix Revolutions", "Spiderman", "John Wick 4", "Avatar 2"];
+  const pollVotes1 = [5, 6, 4, 7];
+  const foodLabels = ["Sushi", "Tacos", "Pizza", "Ramen"];
+  const pollVotes2 = [5, 6, 4, 7];
 
-const ctx = document.getElementById('chart1');
-const ctx2 = document.getElementById('chart2');
+  const ctx = $('#chart1');
+  const ctx2 = $('#chart2');
 
-assignChartParams(movieLabels, pollVotes1, ctx);
-assignChartParams(foodLabels, pollVotes2, ctx2);
+  assignChartParams(movieLabels, pollVotes1, ctx);
+  assignChartParams(foodLabels, pollVotes2, ctx2);
+});
+
 
 
 
