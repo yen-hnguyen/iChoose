74e11e5be68bb75c589d9176577640a911f62c39
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-05 23:07:58 +0000

    set up body-parser

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const morgan = require("morgan");
 
@@ -20,7 +21,7 @@ db.connect();
 app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   "/styles",
