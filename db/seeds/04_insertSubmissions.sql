1a90463351154bb159eaeccbcf8e869f965915c2
vagrant <wong_michael@rogers.com>
2022-02-05 22:03:26 +0000

    Add new data in seeds

--- db/seeds/04_insertSubmissions.sql
+++ db/seeds/04_insertSubmissions.sql
@@ -1,4 +1,5 @@
 
+-- Food
 -- poll submissions for Japanese
 INSERT INTO submissions (choice_id, user_id, point) VALUES (1, 1, 1);
 INSERT INTO submissions (choice_id, user_id, point) VALUES (1, 2, 2);
@@ -13,12 +14,37 @@ INSERT INTO submissions (choice_id, user_id, point) VALUES (2, 4, 1);
 
 -- poll submissions for Chinese
 INSERT INTO submissions (choice_id, user_id, point) VALUES (3, 1, 3);
-INSERT INTO submissions (choice_id, user_id, point) VALUES ( 3, 2, 4);
-INSERT INTO submissions (choice_id, user_id, point) VALUES ( 3, 3, 1);
-INSERT INTO submissions (choice_id, user_id, point) VALUES ( 3, 4, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (3, 2, 4);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (3, 3, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (3, 4, 2);
 
 -- poll submissions for Korean
-INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 1, 4);
-INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 2, 1);
-INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 3, 2);
-INSERT INTO submissions (choice_id, user_id, point) VALUES ( 4, 4, 3);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (4, 1, 4);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (4, 2, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (4, 3, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (4, 4, 3);
+
+--Movies
+-- poll submissions for Action
+INSERT INTO submissions (choice_id, user_id, point) VALUES (5, 5, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (5, 6, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (5, 8, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (5, 8, 4);
+
+-- poll submissions for Drama
+INSERT INTO submissions (choice_id, user_id, point) VALUES (6, 5, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (6, 6, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (6, 7, 3);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (6, 8, 1);
+
+-- poll submissions for Comedy
+INSERT INTO submissions (choice_id, user_id, point) VALUES (7, 5, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (7, 6, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (7, 7, 1);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (7, 8, 1);
+
+-- poll submissions for Romance
+INSERT INTO submissions (choice_id, user_id, point) VALUES (8, 5, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (8, 6, 3);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (8, 7, 2);
+INSERT INTO submissions (choice_id, user_id, point) VALUES (8, 8, 1);
