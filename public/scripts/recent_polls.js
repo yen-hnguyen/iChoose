e233ec1a6353f48c054c7fbcffadbcf304897d5a
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-02-05 11:58:32 -0500

    render poll from db seeds

--- public/scripts/recent_polls.js
+++ public/scripts/recent_polls.js
@@ -2,31 +2,56 @@ import { assignChartParams } from "./chart_params.js"
 
 $(document).ready(function () {
 
-  const polls = $("#chosen-polls");
-  const pollElement = `
+  //GET poll from JSON
+  $.get("/polls", data => {
+
+    const choiceArray = getChoices(data);
+    const pointArray = getPoints(data);
+
+    renderPollData(data.polls[0].description);
+
+    const ctx = $('#chart1');
+    assignChartParams(choiceArray, pointArray, ctx);
+
+    const ctx2 = $('#chart2');
+    assignChartParams(choiceArray, pointArray, ctx2);
+  });
+
+  const getChoices = (data) => {
+    const choiceArray = [];
+    for (const choice of data.polls) {
+      choiceArray.push(choice.choice);
+    }
+    return choiceArray;
+  }
+
+  const getPoints = (data) => {
+    const pointArray = [];
+    for (const point of data.polls) {
+      pointArray.push(Number(point.total_points));
+    }
+    return pointArray;
+  }
+
+  const renderPollData = (data) => {
+    const polls = $("#chosen-polls");
+    const pollElement = `
   <div class="recent-polls" style="width: 425px;">
-   <h5 id="title1">Which movie to watch on the weekend?</h5>
+   <h5 id="title1">${data}</h5>
    <canvas id="chart1" width="400" height="400"></canvas>
   </div>
   <div class="recent-polls" style="width: 425px;">
-   <h5 id="title2">What's the lunch plan on Friday?</h5>
+   <h5 id="title2">${data}</h5>
    <canvas id="chart2" width="400" height="400"></canvas>
   </div>
   `;
-  polls.append(pollElement);
-  polls.css({"display": "flex", "justify-content": "space-around"});
+    polls.append(pollElement);
+    polls.css({ "display": "flex", "justify-content": "space-around" });
+  };
 
-  const movieLabels = ["Matrix Revolutions", "Spiderman", "John Wick 4", "Avatar 2"];
-  const pollVotes1 = [5, 6, 4, 7];
-  const foodLabels = ["Sushi", "Tacos", "Pizza", "Ramen"];
-  const pollVotes2 = [5, 6, 4, 7];
-
-  const ctx = $('#chart1');
-  const ctx2 = $('#chart2');
-
-  assignChartParams(movieLabels, pollVotes1, ctx);
-  assignChartParams(foodLabels, pollVotes2, ctx2);
 });
+
+
 
 
 
