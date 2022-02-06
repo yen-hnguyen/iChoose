7aad314adca471b0e2aa3660cd8bf57778fed552
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-06 03:22:23 +0000

    insert data into polls and choices table upon new poll submission

--- routes/polls.js
+++ routes/polls.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { render } = require('express/lib/response');
 const router = express.Router();
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
+// const { generateRandomString } = require('/scripts/helpers.js');
 
 require('dotenv').config();
 const apiKey = process.env.MAILGUN_API;
@@ -24,21 +26,75 @@ const polls = [{
 }];
 
 module.exports = (db) => {
-
-
   //GET: New Poll link
   router.get("/new", (req, res) => {
     res.render("new_poll");
   });
 
   router.post("/new", (req, res) => {
-    const formData = req.body.text;
+    const data = req.body;
+
+    const userID = 1; // use cookie session to retrieve this
+    const title = data.poll_title;
+    const description = data.description;
+    const email = data.email;
+    const pollOptions = data.poll_options;
+    const admin_link = `http://localhost:8080/polls/result`; // should we add poll id here? random string
+    const submission_link = `http://localhost:8080/polls`;
+
+    const queryString = ` 
+    INSERT INTO polls (user_id, title, description, admin_link, submission_link)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *`;
+  
+    const queryParams = [
+      userID, title, description, admin_link, submission_link
+    ];
+    
+    db.query(queryString, queryParams)
+      .then(data => {
+        console.log(data.rows[0]);
+        return data.rows[0];
+      })
+      .then(poll => {
+        const poll_id = poll.id;
+        const queryParams = [];
+        const queryValues = [];
+        const values = [];
+        let i = 0;
+
+        for (const option of pollOptions) {
+          queryValues.push(option);
+          values.push(`$${queryValues.length}`);
+        }
+        console.log(queryValues);
+        console.log(values);
+        const queryString = `
+        INSERT INTO choices (poll_id, title)
+        VALUES $1`;
+
+        while (i < values.length) {
+          db.query(queryString, [queryValues[i]]);
+          console.log(queryString, [queryValues[i]]);
+          i++;
+        }
+        console.log("I did it");
+      })
+      .then(() => {
+        console.log("Yay");
+        res.render("poll_result");
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
 
   // Result page
-  router.get("/result", (req, res) => {
-    res.render("poll_result");
-  });
+  // router.get("/result", (req, res) => {
+  //   res.render("poll_result");
+  // });
 
   /**
    * Add new poll
@@ -88,7 +144,7 @@ module.exports = (db) => {
   /**
    * Edit: User submits poll
    */
-  router.post("/:id", (req, res) => { });
+  // router.post("/:id", (req, res) => { });
 
   /**
    * Add: User creates a poll
