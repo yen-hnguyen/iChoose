8c11306d472505458f42a340393f6f56aa570a82
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-06 22:21:49 +0000

    add submission link and admin link to database

--- db/seeds/02_insertPolls.sql
+++ db/seeds/02_insertPolls.sql
@@ -1,5 +1,7 @@
 
 --a setting up poll
-INSERT INTO polls (user_id, title, description, admin_link, submission_link) VALUES (1, 'Food', 'What we want to eat', 'http://localhost:8080/administrative', 'http://localhost:8080/submission');
+INSERT INTO polls (user_id, title, description, admin_link, submission_link) VALUES (1, 'Food', 'What we want to eat', 'http://localhost:8080/polls/na5l2y/result', 'http://localhost:8080/polls/na5l2y');
 
-INSERT INTO polls (user_id, title, description, admin_link, submission_link) VALUES (2, 'Movies', 'What movie do you want to watch', 'http://localhost:8080/administrative', 'http://localhost:8080/submission');
+INSERT INTO polls (user_id, title, description, admin_link, submission_link) VALUES (2, 'Movies', 'What movie do you want to watch', 'http://localhost:8080/polls/hmskvg/result', 'http://localhost:8080/polls/hmskvg');
+
+INSERT INTO polls (user_id, title, description, admin_link, submission_link) VALUES (3, 'Summer Trip', 'What movie do you want to watch', 'http://localhost:8080/polls/qjbc6j/result', 'http://localhost:8080/polls/qjbc6j');
