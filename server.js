c43ffab96f37b7adbea12e9d349ea980969f6fdf
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-06 05:36:43 +0000

    add cookie session to project

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(
 app.use(express.static("public"));
 app.use(cookieSession({
   name: 'session',
-  key: [ 'iChoose' ]
+  keys: [ 'iChoose' ]
 }));
 
 // Separated Routes for each Resource
