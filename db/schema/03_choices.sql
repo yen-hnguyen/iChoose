50be97e75a55334badcab5e3e8cc8468444c65f0
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-07 02:40:59 +0000

    update db

--- db/schema/03_choices.sql
+++ db/schema/03_choices.sql
@@ -3,6 +3,4 @@ DROP TABLE IF EXISTS choices CASCADE;
     id SERIAL PRIMARY KEY NOT NULL,
     poll_id INTEGER REFERENCES polls(id),
     title VARCHAR(255) NOT NULL,
-    description TEXT
-
 );
--- db/seeds/03_insertChoices.sql
+++ db/seeds/03_insertChoices.sql
@@ -1,11 +1,16 @@
 
 -- a setting up poll choices
-INSERT INTO choices (poll_id, title, description) VALUES (1, 'Japanese', 'Jabistro');
-INSERT INTO choices (poll_id, title, description) VALUES (1, 'Italian', 'Scaddabush');
-INSERT INTO choices (poll_id, title, description) VALUES (1, 'Chinese', 'Omni Noodles');
-INSERT INTO choices (poll_id, title, description) VALUES (1, 'Korean', 'Nak-Won');
+INSERT INTO choices (poll_id, title) VALUES (1,'Jabistro');
+INSERT INTO choices (poll_id, title) VALUES (1,'Scaddabush');
+INSERT INTO choices (poll_id, title) VALUES (1, 'Omni Noodles');
+INSERT INTO choices (poll_id, title) VALUES (1, 'Nak-Won');
 
-INSERT INTO choices (poll_id, title, description) VALUES (2, 'Action', 'Spiderman');
-INSERT INTO choices (poll_id, title, description) VALUES (2, 'Drama', 'Parasite');
-INSERT INTO choices (poll_id, title, description) VALUES (2, 'Comedy', 'Ted');
-INSERT INTO choices (poll_id, title, description) VALUES (2, 'Romance', 'La La Land');
+INSERT INTO choices (poll_id, title) VALUES (2, 'Spiderman');
+INSERT INTO choices (poll_id, title) VALUES (2, 'Parasite');
+INSERT INTO choices (poll_id, title) VALUES (2, 'Ted');
+INSERT INTO choices (poll_id, title) VALUES (2, 'La La Land');
+
+INSERT INTO choices (poll_id, title) VALUES (3, 'Hawaii');
+INSERT INTO choices (poll_id, title) VALUES (3, 'Tulum');
+INSERT INTO choices (poll_id, title) VALUES (3, 'Bali');
+INSERT INTO choices (poll_id, title) VALUES (3, 'Santorini');
