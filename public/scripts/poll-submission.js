7002de51b8c0fd7a511491f0b557acbb85b65327
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-08 05:29:36 +0000

    update the draggable feature we can get the data to backend

--- public/scripts/poll-submission.js
+++ public/scripts/poll-submission.js
@@ -1,18 +1,12 @@
 /* eslint-disable no-undef */
 $(document).ready(function() {
-  // $.get("/polls/choice", data => {
-  //   console.log("data is", data);
-  //   addElements(data.polls);
-  // });
-  // const addElements = function(data) {
-  //   $("#user-votes").empty().append(
-  //     `<li class="options" id="option1">${data[0].title}</li>
-  //     <li class="options" id="option2">${data[1].title}</li>
-  //     <li class="options" id="option3">${data[2].title}</li>
-  //     <li class="options" id="option4">${data[3].title}</li>`
-  //   );
-  // };
-  $("#user-votes").sortable({
+  $("#sortable").sortable({
     cursor: "move",
+    update: function(event, ui) {
+      let choicesOrder = $(this).sortable('toArray').toString();
+      $('#ranking').val(choicesOrder);
+      console.log(choicesOrder);
+    }
   }).disableSelection();
+
 });
