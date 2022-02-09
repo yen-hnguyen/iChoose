0a17dc58bf1f7cec92634a604e335c9cf4893765
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-09 05:34:01 +0000

    remove unused files

--- public/scripts/helpers.js
+++
@@ -1,6 +0,0 @@
-//function that generates a random string for IDS
-const generateRandomString = function() {
-  return Math.random().toString(36).substring(2,8);
-};
-
-module.exports = { generateRandomString };
